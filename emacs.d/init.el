4daa76948e5853bad54dd5cbfba522e09ee71f11
Kyle <kas2207@columbia.edu>
2016-04-30 23:00:35 -0400

    initializing use-package with helm

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,69 +1,112 @@
 ;; Kyle's Emacs Config
 
-;;;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(eval-and-compile
+  (mapc #'(lambda (path)
+            (add-to-list 'load-path
+                         (expand-file-name path user-emacs-directory)))
+        '("site-lisp" "site-lisp/use-package" "site-lisp/diminish"))
+  (defvar use-package-verbose t)
+  (require 'use-package))
 
-;; Turn off welcome screen
-(setq inhibit-startup-message t)
+(require 'bind-key)
+(require 'diminish nil t)
 
-;; Unicode FTW
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(set-language-environment "UTF-8")
-(setq current-language-environment "UTF-8")
+;;;;;;;;;;;;;;;;;;;
+;; Load Packages ;;
+;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Global Settings
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package helm-config
+  :demand t
+  :load-path "site-lisp/helm"
+  :bind (("C-c h"   . helm-command-prefix)
+         ("M-x"     . helm-M-x)
+         ("C-x C-f" . helm-find-files))
+  :config
+  ;;(use-package helm-commands)
+  ;;(use-package helm-files)
+  ;;(use-package helm-buffers)
+  ;;(use-package helm-mode
+    ;; :diminish helm-mode
+    ;; :init
+    ;; (helm-mode 1))
 
-;; Enable Line and Column Numbering
-(line-number-mode 1)
-(column-number-mode 1)
+  (use-package helm-multi-match)
 
-;; Global Line Numbers
-(global-linum-mode t)
+  (helm-autoresize-mode 1)
 
-;; Turn on font-locking globally
-(global-font-lock-mode t)
+  :bind (:map minibuffer-local-map
+              ("C-c C-l"    . minibuffer-local-map))
 
-;; Unify the emacs kill ring and the clipboad
-(setq x-select-enable-clipboard t)
+  (when (executable-find "curl")
+    (setq helm-google-suggest-use-curl-p t)))
+;; ;;;; Turn off mouse interface early in startup to avoid momentary display
+;; (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
-;; Don't allow automatic tab inserts
-(setq-default indent-tabs-mode nil)
+;; ;; Turn off welcome screen
+;; (setq inhibit-startup-message t)
 
-;; Enable line highlighting
-(global-hl-line-mode 1)
+;; ;; Unicode FTW
+;; (setq locale-coding-system 'utf-8)
+;; (set-terminal-coding-system 'utf-8)
+;; (set-keyboard-coding-system 'utf-8)
+;; (set-selection-coding-system 'utf-8)
+;; (prefer-coding-system 'utf-8)
+;; (set-language-environment "UTF-8")
+;; (setq current-language-environment "UTF-8")
 
-;; Cursor-style setting functions
-(defun set-cursor-type (cursor)
-  "Modify the cursor to the specified type"
-  (interactive "Cursor type (bar, box, etc.): ")
-  (modify-frame-parameters
-   (selected-frame)
-   (list (cons 'cursor-type (intern cursor)))))
+;; ;; Move back-ups to temp directory
+;; (setq backup-directory-alist
+;;       `((".*" . ,temporary-file-directory)))
+;; (setq auto-save-file-name-transforms
+;;       `((".*" ,temporary-file-directory t)))
 
-(defun set-bar-cursor ()
-  "Change the cursor to a bar rather than the (default) box"
-  (interactive)
-  (set-cursor-type "bar"))
+;; ;;;;;;;;;;;;;;;;;;;;;
+;; ;; Global Settings ;;
+;; ;;;;;;;;;;;;;;;;;;;;;
 
-(defun set-box-cursor ()
-  "Change the cursor to a box (the default style)"
-  (interactive)
-  (set-cursor-type "box"))
+;; ;; Enable Line and Column Numbering
+;; (line-number-mode 1)
+;; (column-number-mode 1)
 
-(set-bar-cursor)
+;; ;; Global Line Numbers
+;; (global-linum-mode t)
 
-;; Stop blinking cursor
-(blink-cursor-mode 0)
+;; ;; Turn on font-locking globally
+;; (global-font-lock-mode t)
 
-;; Default text mode
-(setq default-major-mode 'org-mode)
+;; ;; Unify the emacs kill ring and the clipboad
+;; (setq x-select-enable-clipboard t)
+
+;; ;; Don't allow automatic tab inserts
+;; (setq-default indent-tabs-mode nil)
+
+;; ;; Enable line highlighting
+;; (global-hl-line-mode 1)
+
+;; ;; Cursor-style setting functions
+;; (defun set-cursor-type (cursor)
+;;   "Modify the cursor to the specified type"
+;;   (interactive "Cursor type (bar, box, etc.): ")
+;;   (modify-frame-parameters
+;;    (selected-frame)
+;;    (list (cons 'cursor-type (intern cursor)))))
+
+;; (defun set-bar-cursor ()
+;;   "Change the cursor to a bar rather than the (default) box"
+;;   (interactive)
+;;   (set-cursor-type "bar"))
+
+;; (defun set-box-cursor ()
+;;   "Change the cursor to a box (the default style)"
+;;   (interactive)
+;;   (set-cursor-type "box"))
+
+;; (set-bar-cursor)
+
+;; ;; Stop blinking cursor
+;; (blink-cursor-mode 0)
+
+;; ;; Default text mode
+;; (setq default-major-mode 'org-mode)
