f51b6f24aace0ff312c04bc9bb2130a2e073595f
Kyle <kas2207@columbia.edu>
2016-05-02 23:47:24 -0400

    Creating a settings package which is where the default emacs custom settings will live

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,6 +11,9 @@
 (require 'bind-key)
 (require 'diminish nil t)
 
+(setq custom-file (locate-user-emacs-file "settings.el"))
+(load custom-file)
+
 ;;;;;;;;;;;;;;;;;;;
 ;; Load Packages ;;
 ;;;;;;;;;;;;;;;;;;;
@@ -55,74 +58,3 @@
   
   (when (executable-find "curl")
     (setq helm-google-suggest-use-curl-p t)))
-;; ;;;; Turn off mouse interface early in startup to avoid momentary display
-;; (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-
-;; ;; Turn off welcome screen
-;; (setq inhibit-startup-message t)
-
-;; ;; Unicode FTW
-;; (setq locale-coding-system 'utf-8)
-;; (set-terminal-coding-system 'utf-8)
-;; (set-keyboard-coding-system 'utf-8)
-;; (set-selection-coding-system 'utf-8)
-;; (prefer-coding-system 'utf-8)
-;; (set-language-environment "UTF-8")
-;; (setq current-language-environment "UTF-8")
-
-;; ;; Move back-ups to temp directory
-;; (setq backup-directory-alist
-;;       `((".*" . ,temporary-file-directory)))
-;; (setq auto-save-file-name-transforms
-;;       `((".*" ,temporary-file-directory t)))
-
-;; ;;;;;;;;;;;;;;;;;;;;;
-;; ;; Global Settings ;;
-;; ;;;;;;;;;;;;;;;;;;;;;
-
-;; ;; Enable Line and Column Numbering
-;; (line-number-mode 1)
-;; (column-number-mode 1)
-
-;; ;; Global Line Numbers
-;; (global-linum-mode t)
-
-;; ;; Turn on font-locking globally
-;; (global-font-lock-mode t)
-
-;; ;; Unify the emacs kill ring and the clipboad
-;; (setq x-select-enable-clipboard t)
-
-;; ;; Don't allow automatic tab inserts
-;; (setq-default indent-tabs-mode nil)
-
-;; ;; Enable line highlighting
-;; (global-hl-line-mode 1)
-
-;; ;; Cursor-style setting functions
-;; (defun set-cursor-type (cursor)
-;;   "Modify the cursor to the specified type"
-;;   (interactive "Cursor type (bar, box, etc.): ")
-;;   (modify-frame-parameters
-;;    (selected-frame)
-;;    (list (cons 'cursor-type (intern cursor)))))
-
-;; (defun set-bar-cursor ()
-;;   "Change the cursor to a bar rather than the (default) box"
-;;   (interactive)
-;;   (set-cursor-type "bar"))
-
-;; (defun set-box-cursor ()
-;;   "Change the cursor to a box (the default style)"
-;;   (interactive)
-;;   (set-cursor-type "box"))
-
-;; (set-bar-cursor)
-
-;; ;; Stop blinking cursor
-;; (blink-cursor-mode 0)
-
-;; ;; Default text mode
-;; (setq default-major-mode 'org-mode)
---
+++ emacs.d/settings.el
@@ -0,0 +1,72 @@
+;;;;;;;;;;;;;;;
+;; Customize ;;
+;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;; Turn off welcome screen
+(setq inhibit-startup-message t)
+
+;; Unicode FTW
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-language-environment "UTF-8")
+(setq current-language-environment "UTF-8")
+
+;; Move back-ups to temp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;;;;;;;;;;;;;;;;;;;;;
+;; Global Settings ;;
+;;;;;;;;;;;;;;;;;;;;;
+
+;; Global Line Numbers
+(global-linum-mode t)
+
+;; Turn on font-locking globally
+(global-font-lock-mode t)
+
+;; Unify the emacs kill ring and the clipboad
+(setq x-select-enable-clipboard t)
+
+;; Cursor-style setting functions
+(defun set-cursor-type (cursor)
+  "Modify the cursor to the specified type"
+  (interactive "Cursor type (bar, box, etc.): ")
+  (modify-frame-parameters
+   (selected-frame)
+   (list (cons 'cursor-type (intern cursor)))))
+
+(defun set-bar-cursor ()
+  "Change the cursor to a bar rather than the (default) box"
+  (interactive)
+  (set-cursor-type "bar"))
+
+(defun set-box-cursor ()
+  "Change the cursor to a box (the default style)"
+  (interactive)
+  (set-cursor-type "box"))
+
+(set-box-cursor)
+
+;; Stop blinking cursor
+(blink-cursor-mode 0)
