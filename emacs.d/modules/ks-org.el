5ad619d8791b052386f0ac34b211b2f6de86c8c2
kyleschmidt <kyleschmidt@debian.Godel>
2016-05-22 17:33:31 -0400

    init experimenting with org-agenda. Setting up Dropbox.

--- emacs.d/modules/ks-org.el
+++ emacs.d/modules/ks-org.el
@@ -11,7 +11,11 @@
      (visual-line-mode 1)))
   (use-package org-bullets
     :config
-    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))))
-
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (setq org-agenda-files
+	(delq nil
+	      (mapcar (lambda (x) (and (file-exists-p x) x))
+		      '("~/Dropbox/Personal/organizer.org"
+			"~/Dropbox/Personal/business.org")))))
 
 (provide 'ks-org)
--- emacs.d/modules/ks-snippets.el
+++ emacs.d/modules/ks-snippets.el
@@ -6,6 +6,9 @@
   :config (yas-global-mode 1)
   :diminish yas-minor-mode)
 
+(add-hook 'term-mode-hook (lambda()
+        (setq yas-dont-activate t)))
+
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 
 (provide 'ks-snippets)
