166028a08a8b3527365989f548ed1e770f3bc7e0
kyleschmidt <kyleschmidt@debian.Godel>
2016-05-22 18:31:55 -0400

    annoying automatic customization addition.

--- emacs.d/ks-custom.el
+++ emacs.d/ks-custom.el
@@ -6,6 +6,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
     (yasnippet s flx-ido ido-vertical-mode smex ido-ubiquitous helm-flx swiper-helm org-bullets org-plus-contrib magit helm use-package paradox))))
