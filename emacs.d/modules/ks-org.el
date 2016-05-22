c703c46b36f4a665c6e015b5b8028f3b29791e4b
kyleschmidt <kyleschmidt@debian.Godel>
2016-05-22 17:41:19 -0400

    Moving org-agenda-files to init instead of config.

--- emacs.d/modules/ks-org.el
+++ emacs.d/modules/ks-org.el
@@ -1,5 +1,10 @@
 (use-package org
   :ensure org-plus-contrib
+  :init (setq org-agenda-files
+	      (delq nil
+		    (mapcar (lambda (x) (and (file-exists-p x) x))
+			    '("~/Dropbox/Personal/organizer.org"
+			      "~/Dropbox/Personal/business.org"))))
   :bind (("C-c l" . org-store-link)
 	 ("C-c a" . org-agenda)
 	 ("C-c c" . org-capture)
@@ -11,11 +16,6 @@
      (visual-line-mode 1)))
   (use-package org-bullets
     :config
-    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-  (setq org-agenda-files
-	(delq nil
-	      (mapcar (lambda (x) (and (file-exists-p x) x))
-		      '("~/Dropbox/Personal/organizer.org"
-			"~/Dropbox/Personal/business.org")))))
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))))
 
 (provide 'ks-org)
