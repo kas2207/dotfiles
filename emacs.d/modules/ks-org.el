e1cf77d1b222c913aebffa7800938fe0f4315070
kas2207 <kas2207@columbia.edu>
2016-05-23 08:11:48 -0400

    Adding custom sequencing and color faces to todo keywords.

--- emacs.d/modules/ks-org.el
+++ emacs.d/modules/ks-org.el
@@ -12,8 +12,15 @@
 			      ("education" . ?r)
 			      ("finance"   . ?f)
 			      ("phone"     . ?p))
-	      org-default-notes-file
-	      "~/Dropbox/Org-Files/notes.org")
+	      org-default-notes-file "~/Dropbox/Org-Files/notes.org"
+	      org-directory "~/Dropbox/Org-Files"
+	      org-mobile-inbox-for-pull "~/Dropbox/Org-Files/mobile-flagged.org"
+	      org-mobile-directory "~/Dropbox/Apps/MobileOrg"
+	      org-todo-keywords '((sequence "BLOCKED(b)" "TODO(t)" "INPROGRESS(i)" "COMPLETED(c)"))
+	      org-todo-keyword-faces '(("BLOCKED"    . (:foreground "black" :weight bold))
+				       ("TODO"       . org-todo)
+				       ("INPROGRESS" . (:foreground "orange" :background "yellow" :weight bold))
+				       ("COMPLETED"  . org-done)))
   :bind (("C-c l" . org-store-link)
 	 ("C-c a" . org-agenda)
 	 ("C-c c" . org-capture)
