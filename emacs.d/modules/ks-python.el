4d7d85df4c024b9e8a77ecadbf963cb90018f926
kas2207 <kas2207@columbia.edu>
2016-06-21 12:30:01 -0400

    Adding virtualenvwrapper to python mode module.

--- emacs.d/modules/ks-python.el
+++ emacs.d/modules/ks-python.el
@@ -1,6 +1,14 @@
 ;; Python mode
 
+(use-package virtualenvwrapper
+  :config
+  (require 'virtualenvwrapper)
+  (venv-initialize-interactive-shells)
+  (venv-initialize-eshell)
+  (setq venv-location '("~/kyleschmidt/SourceCode/kip-create-api/")))
+
 (setq python-shell-interpreter "python3"
       python-shell-interpreter-args "-i")
+
 
 (provide 'ks-python)
