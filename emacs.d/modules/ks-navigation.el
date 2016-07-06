d90c08da1124e931817dca2bc972570d7da3ea53
kas2207 <kas2207@columbia.edu>
2016-07-06 14:24:19 -0400

    adding quick switch to previous buffer.

--- emacs.d/modules/ks-navigation.el
+++ emacs.d/modules/ks-navigation.el
@@ -32,5 +32,12 @@
          ("C-M-%" . anzu-query-replace-regexp))
   :diminish anzu-mode)
 
+(defun switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(global-set-key (kbd "C-c b") 'switch-to-previous-buffer)
 
 (provide 'ks-navigation)
