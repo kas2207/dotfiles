e5a9af00bf3c42264b3a4c9dbbc41c8012df1acb
kyleschmidt <kyleschmidt@debian.Godel>
2016-12-02 14:33:47 -0500

    modifying hydra for multiple cursors

--- emacs.d/modules/ks-hydra.el
+++ emacs.d/modules/ks-hydra.el
@@ -2,6 +2,40 @@
   :init
   (winner-mode))
 
+(require 'windmove)
+
+(defun hydra-move-splitter-left (arg)
+  "Move window splitter left."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+    (windmove-find-other-window 'right))
+      (shrink-window-horizontally arg)
+    (enlarge-window-horizontally arg)))
+
+(defun hydra-move-splitter-right (arg)
+  "Move window splitter right."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+    (windmove-find-other-window 'right))
+      (enlarge-window-horizontally arg)
+    (shrink-window-horizontally arg)))
+
+(defun hydra-move-splitter-up (arg)
+  "Move window splitter up."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+    (windmove-find-other-window 'up))
+      (enlarge-window arg)
+    (shrink-window arg)))
+
+(defun hydra-move-splitter-down (arg)
+  "Move window splitter down."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+    (windmove-find-other-window 'up))
+      (shrink-window arg)
+    (enlarge-window arg)))
+
 (use-package hydra
   :config
   (defhydra hydra-zoom (global-map "<f2>")
@@ -58,4 +92,26 @@
                                 ("K" hydra-move-splitter-up)
                                 ("L" hydra-move-splitter-right)
 
-                                ("q" nil))))
+                                ("q" nil)))
+  (global-set-key (kbd "M-c")
+                  (defhydra multiple-cursors-hydra (:hint nil)
+                    "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+       ("l" mc/edit-lines :exit t)
+       ("a" mc/mark-all-like-this :exit t)
+       ("n" mc/mark-next-like-this)
+       ("N" mc/skip-to-next-like-this)
+       ("M-n" mc/unmark-next-like-this)
+       ("p" mc/mark-previous-like-this)
+       ("P" mc/skip-to-previous-like-this)
+       ("M-p" mc/unmark-previous-like-this)
+       ("r" mc/mark-all-in-region-regexp :exit t)
+       ("q" nil))))
+
+(provide 'ks-hydra)
