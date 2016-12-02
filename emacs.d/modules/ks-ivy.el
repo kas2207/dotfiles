23fd0fa2c2a9153b4928f4892844615793451685
Kyle Schmidt <kyle.a.schmidt@gmail.com>
2016-12-02 16:28:57 -0500

    modifying ks-ivy

--- emacs.d/modules/ks-ivy.el
+++ emacs.d/modules/ks-ivy.el
@@ -1,24 +1,22 @@
+(use-package counsel
+  :ensure t)
+
 (use-package swiper
-  :diminish ivy-mode
+  :ensure try
+  :init (ivy-mode 1)
   :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (global-set-key (kbd "C-s") 'swiper)
-    (global-set-key (kbd "C-c C-r") 'ivy-resume)
-    (global-set-key (kbd "<f6>") 'ivy-resume)
-    (global-set-key (kbd "M-x") 'counsel-M-x)
-    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-    (global-set-key (kbd "<f1> l") 'counsel-load-library)
-    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-    (global-set-key (kbd "C-c g") 'counsel-git)
-    (global-set-key (kbd "C-c j") 'counsel-git-grep)
-    (global-set-key (kbd "C-c k") 'counsel-ag)
-    (global-set-key (kbd "C-x l") 'counsel-locate)
-    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)))
+  (setq ivy-use-virtual-buffers t)
+  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+  :bind
+  ("C-s"      . swiper)
+  ("C-c C-r"  . ivy-resume)
+  ("<f6>"     . ivy-resume)
+  ("M-x"      . counsel-M-x)
+  ("C-x C-f"  . counsel-find-file)
+  ("C-c g"    . counsel-git)
+  ("C-c j"    . counsel-git-grep)
+  ("C-c k"    . counsel-ag)
+  ("C-x l"    . counsel-locate))
+
 
 (provide 'ks-ivy)
