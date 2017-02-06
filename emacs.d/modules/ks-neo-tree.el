639b54012fd18ae29bc9b5c522622c366094dfb2
kas2207 <kas2207@columbia.edu>
2017-02-06 17:39:22 -0500

    adding neo tree to emacs config

---
+++ emacs.d/modules/ks-neo-tree.el
@@ -0,0 +1,22 @@
+(use-package neotree
+  :config
+  (global-set-key [f5] 'ks/neotree-project-dir)
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-smart-open t)
+  (setq neo-hidden-regexp-list '("^\\." "\\.cs\\.meta$" "\\.pyc$" "~$" "^#.*#$" "\\.elc$" "__pycache__" "venv")))
+
+(defun ks/neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+        (file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+        (if (neo-global--window-exists-p)
+            (progn
+              (neotree-dir project-dir)
+              (neotree-find file-name)))
+      (message "Could not find git project root."))))
+
+
+(provide 'ks-neo-tree)
