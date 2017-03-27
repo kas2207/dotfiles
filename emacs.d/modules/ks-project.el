9b99904667b3070f417e9554c129f1808562e856
k-schmidt <kyle.a.schmidt@gmail.com>
2017-03-27 09:36:32 -0400

    aliasing C-x C-b to find buffers in projects easier

--- emacs.d/modules/ks-project.el
+++ emacs.d/modules/ks-project.el
@@ -6,4 +6,13 @@
   :bind ("C-c C-f" . projectile-find-file)
   :diminish projectile-mode)
 
+(use-package ibuffer-projectile
+  :bind ("C-x C-b"  . ibuffer)
+  :config
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-projectile-set-filter-groups)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic)))))
+
 (provide 'ks-project)
